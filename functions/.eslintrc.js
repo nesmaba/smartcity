24dfa9b144134ff28907f30fa51265386bf78bac
nesmaba <nesmaba@gmail.com>
2021-12-16 12:09:10 +0100

    Funcionando las pruebas. Deploy hecho.

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -10,5 +10,10 @@ module.exports = {
   ],
   rules: {
     quotes: ["error", "double"],
+    indent: ["off"],
+    "max-len": ["off"],
+  },
+  parserOptions: {
+    ecmaVersion: 8, // or 2017
   },
 };
